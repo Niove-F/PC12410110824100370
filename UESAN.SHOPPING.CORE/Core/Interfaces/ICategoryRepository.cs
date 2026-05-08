290d4bd0f8f7ad1cac31f48bb332063357a0e212
Kiara Ruiz <ruizk0169@gmail.com>
2026-05-07 20:50:02 -0500

    SHOPPING

---
+++ UESAN.SHOPPING.CORE/Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using UESAN.SHOPPING.CORE.Core.Entities;
+
+namespace UESAN.SHOPPING.CORE.Core.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task DeleteCategory(int id);
+        Task<IEnumerable<Category>> GetCategories();
+        Task<Category> GetCategoryById(int id);
+        Task UpdateCategory(Category category);
+    }
+}
--- UESAN.SHOPPING.CORE/Infrastructure/Repositories/CategoryRepository.cs
+++ UESAN.SHOPPING.CORE/Infrastructure/Repositories/CategoryRepository.cs
@@ -5,10 +5,11 @@ using System.Text;
 using UESAN.SHOPPING.CORE.Core.Entities;
 using UESAN.SHOPPING.CORE.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using UESAN.SHOPPING.CORE.Core.Interfaces;
 
 namespace UESAN.SHOPPING.CORE.Infrastructure.Repositories
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
         private readonly StoreDbContext _context;
 
@@ -17,30 +18,30 @@ namespace UESAN.SHOPPING.CORE.Infrastructure.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Category>> GetCategories() 
+        public async Task<IEnumerable<Category>> GetCategories()
         {
             return await _context.Category.ToListAsync();
         }
         //Create Category
-        public async Task<Category> GetCategoryById(int id) 
+        public async Task<Category> GetCategoryById(int id)
         {
-            return await _context.Category.Where(c=> c.Id ==id).FirstOrDefaultAsync();
+            return await _context.Category.Where(c => c.Id == id).FirstOrDefaultAsync();
         }
         //Update Category
-        public async Task UpdateCategory(Category category) 
+        public async Task UpdateCategory(Category category)
         {
             var existingCategory = await _context.Category.Where(category => category.Id == category.Id).FirstOrDefaultAsync();
-            if (existingCategory != null) 
+            if (existingCategory != null)
             {
                 existingCategory.Description = category.Description;
                 await _context.SaveChangesAsync();
             }
         }
         //Delete Category
-        public async Task DeleteCategory(int id) 
+        public async Task DeleteCategory(int id)
         {
             var existingCategory = await _context.Category.Where(c => c.Id == id).FirstOrDefaultAsync();
-            if (existingCategory != null) 
+            if (existingCategory != null)
             {
                 existingCategory.IsActive = false;
                 await _context.SaveChangesAsync();
