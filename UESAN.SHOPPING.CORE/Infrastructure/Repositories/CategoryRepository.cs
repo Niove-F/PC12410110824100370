59d6f4815b2f72066fd3bbc1a5bd22ada5a429d7
Kiara Ruiz <ruizk0169@gmail.com>
2026-05-07 19:29:37 -0500

    Agregar archivos de proyecto.

---
+++ UESAN.SHOPPING.CORE/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UESAN.SHOPPING.CORE.Core.Entities;
+using UESAN.SHOPPING.CORE.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace UESAN.SHOPPING.CORE.Infrastructure.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public CategoryRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories() 
+        {
+            return await _context.Category.ToListAsync();
+        }
+        //Create Category
+        public async Task<Category> GetCategoryById(int id) 
+        {
+            return await _context.Category.Where(c=> c.Id ==id).FirstOrDefaultAsync();
+        }
+        //Update Category
+        public async Task UpdateCategory(Category category) 
+        {
+            var existingCategory = await _context.Category.Where(category => category.Id == category.Id).FirstOrDefaultAsync();
+            if (existingCategory != null) 
+            {
+                existingCategory.Description = category.Description;
+                await _context.SaveChangesAsync();
+            }
+        }
+        //Delete Category
+        public async Task DeleteCategory(int id) 
+        {
+            var existingCategory = await _context.Category.Where(c => c.Id == id).FirstOrDefaultAsync();
+            if (existingCategory != null) 
+            {
+                existingCategory.IsActive = false;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
